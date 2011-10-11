be0e877e9efc38313881d5bcb05ad0ed7aa41012
ihuang <ihuang@53513f66-9fe0-4f4b-a901-10d5c8ad6cc7>
2011-10-11 01:31:51 +0000

    TTN 900 - added new interface IPrintStyle

---
+++ pace-base/src/main/java/com/pace/base/view/IPrintStyle.java
@@ -0,0 +1,8 @@
+package com.pace.base.view;
+
+public interface IPrintStyle {
+	public abstract String getGUID();
+	public abstract void setGUID(String GUID);
+	public abstract String getName();
+	public abstract void setName(String name);
+}
