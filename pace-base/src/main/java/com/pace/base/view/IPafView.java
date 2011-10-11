049b760aa149c7761ced1e946c288c68b2d54e94
ihuang <ihuang@53513f66-9fe0-4f4b-a901-10d5c8ad6cc7>
2011-10-11 01:32:33 +0000

    TTN 900 - added new functions for expanded print settings into View

--- pace-base/src/main/java/com/pace/base/view/IPafView.java
+++ pace-base/src/main/java/com/pace/base/view/IPafView.java
@@ -1,5 +1,8 @@
 package com.pace.base.view;
 
+import com.pace.base.ViewPrintState;
+import com.pace.base.ui.PrintStyle;
+
 public interface IPafView {
 
 	public abstract String getDesc();
@@ -22,10 +25,21 @@ public interface IPafView {
 	 * @return Returns the backGroundColor.
 	 */
 	public abstract String getBackGroundColor();
-
+	
 	/**
 	 * @param backGroundColor The backGroundColor to set.
 	 */
 	public abstract void setBackGroundColor(String backGroundColor);
+	
+	public PrintStyle getPrintStyle();
+	
+	public void setPrintStyle( PrintStyle printStyle );
+	
+	public String getGlobalPrintStyleGUID();
+	
+	public void setGlobalPrintStyleGUID( String guidGlobalPrintStyle );
 
+	public abstract ViewPrintState getViewPrintState();
+
+	public abstract void setViewPrintState(ViewPrintState printState);
 }
