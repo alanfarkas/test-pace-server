1cd103adb5d7ef1141e7853f6314ca1f75c97529
jmilliron <jmilliron@53513f66-9fe0-4f4b-a901-10d5c8ad6cc7>
2011-10-25 16:50:20 +0000

    updated name spaces and apache cxf 2.3.7

--- pace-server/pafwsclientcompile-cxf.bat
+++ pace-server/pafwsclientcompile-cxf.bat
@@ -4,5 +4,5 @@ set class_dir=%proj_root%\WebRoot\WEB-INF\classes
 set lib_dir=%proj_root%\WebRoot\WEB-INF\lib
 set client_dir=%proj_root%\client\classes
 
-\java\userlib\apache-cxf-2.2.5\bin\wsdl2java.bat -d %client_dir% -compile -client -p "com.palladium.paf.server.client" http://localhost:8080/PafServer/PafService?wsdl
+\proj_wrksp\userlib\apache-cxf-2.3.7\bin\wsdl2java.bat -d %client_dir% -compile -client -p "com.pace.server.client" http://localhost:8080/pace/PafService?wsdl
 
