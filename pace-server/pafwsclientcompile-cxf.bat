f96d02726b1dac2f8642ef3238149435f2826417
jmilliron <jmilliron@53513f66-9fe0-4f4b-a901-10d5c8ad6cc7>
2011-11-08 20:40:06 +0000

    updated PafService to app

--- pace-server/pafwsclientcompile-cxf.bat
+++ pace-server/pafwsclientcompile-cxf.bat
@@ -4,5 +4,5 @@ set class_dir=%proj_root%\WebRoot\WEB-INF\classes
 set lib_dir=%proj_root%\WebRoot\WEB-INF\lib
 set client_dir=%proj_root%\client\classes
 
-\proj_wrksp\userlib\apache-cxf-2.3.7\bin\wsdl2java.bat -d %client_dir% -compile -client -p "com.pace.server.client" http://localhost:8080/pace/PafService?wsdl
+\proj_wrksp\userlib\apache-cxf-2.3.7\bin\wsdl2java.bat -d %client_dir% -compile -client -p "com.pace.server.client" http://localhost:8080/pace/app?wsdl
 
