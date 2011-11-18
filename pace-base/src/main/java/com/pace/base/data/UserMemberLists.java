f2394f39815b91e88c6e3ca4496f908b4660de5e
jwatkins <jwatkins@53513f66-9fe0-4f4b-a901-10d5c8ad6cc7>
2011-11-18 01:53:32 +0000

    New construct for holding member lists

---
+++ pace-base/src/main/java/com/pace/base/data/UserMemberLists.java
@@ -0,0 +1,10 @@
+package com.pace.base.data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserMemberLists {
+
+	private Map<String, PafMemberList> userMemberLists = new HashMap<String, PafMemberList>();
+	
+}
