78590439af02efa1ceae5ac1cd6530b9867e3e11
afarkas <afarkas@53513f66-9fe0-4f4b-a901-10d5c8ad6cc7>
2011-12-16 10:01:34 +0000

    OPEN - issue TTN-1598: Offset Versions http://ams.pace-planning.com/jira/browse/TTN-1598

---
+++ pace-base/src/main/java/com/pace/base/mdb/DataBlockKeyAliasType.java
@@ -0,0 +1,13 @@
+/**
+ * Data Block Key Alias Type (Non-Mutually Exclusive Property)
+ */
+package com.pace.base.mdb;
+
+/**
+ * @author Alan Farkas
+ *
+ */
+public enum DataBlockKeyAliasType {
+	Attribute,
+	OffsetVersion;
+}
