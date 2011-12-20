61802ab7b09ad52fd2371973387e1d588c4674d1
jmilliron <jmilliron@53513f66-9fe0-4f4b-a901-10d5c8ad6cc7>
2011-12-20 22:25:54 +0000

    Key value object used in vaadin to represent map item

---
+++ pace-base/src/main/java/com/pace/base/misc/KeyValue.java
@@ -0,0 +1,97 @@
+/**
+ * 
+ */
+package com.pace.base.misc;
+
+/**
+ * Key Value object.
+ * 
+ * @author JMilliron
+ *
+ */
+public class KeyValue {
+
+	private String key;
+	
+	private String value;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param key key 
+	 * @param value value
+	 */
+	public KeyValue(String key, String value) {
+		this.key = key;
+		this.value = value;		
+	}
+
+	/**
+	 * @return the key
+	 */
+	public String getKey() {
+		return key;
+	}
+
+	/**
+	 * @param key the key to set
+	 */
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public String getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "KeyValue [key=" + key + ", value=" + value + "]";
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((key == null) ? 0 : key.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		KeyValue other = (KeyValue) obj;
+		if (key == null) {
+			if (other.key != null)
+				return false;
+		} else if (!key.equals(other.key))
+			return false;
+		return true;
+	}
+	
+	
+}
