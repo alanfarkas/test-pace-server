5c9fa78239696b81fa7ce8c3dbf7e99ffbd8948c
ihuang <ihuang@53513f66-9fe0-4f4b-a901-10d5c8ad6cc7>
2012-03-14 16:03:31 +0000

    updated userlib for ant script for creating wsdl from java using cxf utility

--- pace-server/pafwsclientcompile-cxf-wsdl2java.bat
+++ pace-server/pafwsclientcompile-cxf-wsdl2java.bat
@@ -1,8 +1,8 @@
 set proj_root=%1
-
+set user_lib=C:\java\userlib
 set class_dir=%proj_root%\WebRoot\WEB-INF\classes
 set lib_dir=%proj_root%\WebRoot\WEB-INF\lib
 set client_dir=%proj_root%\client\classes
 
-\proj_wrksp\userlib\apache-cxf-2.4.6\bin\wsdl2java.bat -d %client_dir% -compile -client -p "com.pace.server.client" %proj_root%\WebRoot\WEB-INF\pafservice.wsdl
+%user_lib%\apache-cxf-2.4.6\bin\wsdl2java.bat -d %client_dir% -compile -client -p "com.pace.server.client" %proj_root%\WebRoot\WEB-INF\pafservice.wsdl
 
