bd11bb1e81f0a3f36723f2e4574949c071d8448d
ihuang <ihuang@53513f66-9fe0-4f4b-a901-10d5c8ad6cc7>
2012-03-14 13:33:07 +0000

    added ant script for creating wsdl from java using cxf utility

---
+++ pace-server/pafwsclientcompile-cxf-java2wsdl.bat
@@ -0,0 +1,9 @@
+set proj_root=%1
+
+set class_dir=%proj_root%\WebRoot\WEB-INF\classes
+set dist_dir=%proj_root%\client\dist
+set mavenlib_dir=%USERPROFILE%\.m2\repository
+
+\proj_wrksp\userlib\apache-cxf-2.4.6\bin\java2ws.bat -wsdl -o %proj_root%\WebRoot\WEB-INF\pafservice.wsdl -cp %class_dir%;%dist_dir%\pace-base.jar;%mavenlib_dir%\log4j\log4j\1.2.16\log4j-1.2.16.jar;%mavenlib_dir%\org\nfunk\jep\jep-main\2.4\jep-main-2.4.jar com.pace.server.PafServiceProvider
+
+
