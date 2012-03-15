7c4a81cf71ccd19a5580bdf71d3791f6f05914fa
ihuang <ihuang@53513f66-9fe0-4f4b-a901-10d5c8ad6cc7>
2012-03-15 01:53:03 +0000

    updated ant script for creating wsdl from java using cxf utility

--- pace-server/pafwsclientcompile-cxf-java2wsdl.bat
+++ pace-server/pafwsclientcompile-cxf-java2wsdl.bat
@@ -4,6 +4,6 @@ set class_dir=%proj_root%\WebRoot\WEB-INF\classes
 set dist_dir=%proj_root%\client\dist
 set mavenlib_dir=%USERPROFILE%\.m2\repository
 
-%user_lib%\apache-cxf-2.4.6\bin\java2ws.bat -wsdl -o %proj_root%\WebRoot\WEB-INF\pafservice.wsdl -cp %class_dir%;%dist_dir%\pace-base.jar;%mavenlib_dir%\log4j\log4j\1.2.16\log4j-1.2.16.jar;%mavenlib_dir%\org\nfunk\jep\jep-main\2.4\jep-main-2.4.jar com.pace.server.PafServiceProvider
+%user_lib%\apache-cxf-2.4.6\bin\java2ws.bat -wsdl -o %proj_root%\WebRoot\WEB-INF\PafServiceProviderService.wsdl -address http://localhost:8080/pace/PafServiceProvider -cp %dist_dir%\pace-base.jar;%class_dir%;%mavenlib_dir%\log4j\log4j\1.2.16\log4j-1.2.16.jar;%mavenlib_dir%\org\nfunk\jep\jep-main\2.4\jep-main-2.4.jar com.pace.server.PafServiceProvider
 
 
--- pace-server/pafwsclientcompile-cxf-wsdl2java.bat
+++ pace-server/pafwsclientcompile-cxf-wsdl2java.bat
@@ -4,5 +4,5 @@ set class_dir=%proj_root%\WebRoot\WEB-INF\classes
 set lib_dir=%proj_root%\WebRoot\WEB-INF\lib
 set client_dir=%proj_root%\client\classes
 
-%user_lib%\apache-cxf-2.4.6\bin\wsdl2java.bat -d %client_dir% -compile -client -p "com.pace.server.client" %proj_root%\WebRoot\WEB-INF\pafservice.wsdl
+%user_lib%\apache-cxf-2.4.6\bin\wsdl2java.bat -d %client_dir% -compile -client -p "com.pace.server.client" %proj_root%\WebRoot\WEB-INF\PafServiceProviderService.wsdl
 
