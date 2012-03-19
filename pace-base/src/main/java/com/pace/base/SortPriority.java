f65cb8d1560cdd21db71c26dbde024303a9c49d4
ihuang <ihuang@53513f66-9fe0-4f4b-a901-10d5c8ad6cc7>
2012-03-19 01:40:17 +0000

    Added index property to SortPriority

--- pace-base/src/main/java/com/pace/base/SortPriority.java
+++ pace-base/src/main/java/com/pace/base/SortPriority.java
@@ -8,5 +8,23 @@ package com.pace.base;
  *
  */
 public enum SortPriority {
-	Primary, Secondary, Tertiary
+	Primary(1), 
+	Secondary(2), 
+	Tertiary(3);
+	
+	private int index;
+	
+	public int getIndex() {
+		return index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	SortPriority() {}
+	
+	SortPriority(int index) {
+		this.index = index;
+	}
 }
