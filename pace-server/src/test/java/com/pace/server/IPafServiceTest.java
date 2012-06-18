4319e9c3f770537824e64549a67d1962b98da3c6
ihuang <ihuang@53513f66-9fe0-4f4b-a901-10d5c8ad6cc7>
2012-06-18 20:16:04 +0000

    Moving Junit tests to test folder

---
+++ pace-server/src/test/java/com/pace/server/IPafServiceTest.java
@@ -0,0 +1,30 @@
+package com.pace.server;
+
+import junit.framework.TestCase;
+
+public class IPafServiceTest extends TestCase {
+
+    /*
+     * Test method for 'com.pace.base.server.IPafService.getVersion()'
+     */
+    public void testGetVersion() {
+        
+
+    }
+
+    /*
+     * Test method for 'com.pace.base.server.IPafService.getView(String)'
+     */
+    public void testGetView() {
+
+    }
+
+    /*
+     * Test method for 'com.pace.base.server.IPafService.saveViewCache()'
+     */
+    public void testSaveViewCache() {
+        
+
+    }
+
+}
